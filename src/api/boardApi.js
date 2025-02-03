14092a21b304cc3e18da244fd11888efef917990
Nikhil kumar saha <nikhilsaha92236@gmail.com>
2025-02-03 13:39:12 +0530

    deployment integration

--- src/api/boardApi.js
+++ src/api/boardApi.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = process.env.VITE_BACKEND_URI;
 
 export const fetchBoard = async () => {
   const response = await fetch(`${API_BASE_URL}/board`);
